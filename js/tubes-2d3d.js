0f7ed5fa1138a289b9d161a87a8376417093e80e
Jan Horava <czechinthemail@gmail.com>
2015-11-16 21:56:11 -0800

    fixed step error

--- js/tubes-2d3d.js
+++ js/tubes-2d3d.js
@@ -372,7 +372,7 @@ function setmode(newmode){
     document.body.innerHTML = '<div><div><a onclick=setmode(1)>Enter the Tubes</a></div></div><div class=right><div><input disabled value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
       + settings['movement-keys'] + '>Move ←→<br><input id=key-slowdown maxlength=1 value='
       + settings['key-slowdown'] + '>Speed--<br><input id=key-speedup maxlength=1 value='
-      + settings['key-speedup'] + '>Speed++</div><hr><div><input id=audio-volume max=1 min=0 step=".01" type=range value='
+      + settings['key-speedup'] + '>Speed++</div><hr><div><input id=audio-volume max=1 min=0 step=0.01 type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=ms-per-frame value='
       + settings['ms-per-frame'] + '>ms/Frame<br><a onclick=reset()>Reset Settings</a></div></div>';
 }
