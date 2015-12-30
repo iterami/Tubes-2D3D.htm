5118b7c95495881a73301d227be80baea485606d
Jan Horava <czechinthemail@gmail.com>
2015-12-30 01:51:31 -0800

    simplified save()

--- js/tubes-2d3d.js
+++ js/tubes-2d3d.js
@@ -281,10 +281,9 @@ function save(){
       'movement-keys': 'AD',
     };
     for(var id in ids){
-        var value = document.getElementById(id).value;
-        settings[id] = value;
+        settings[id] = document.getElementById(id).value;
 
-        if(value === ids[id]){
+        if(settings[id] === ids[id]){
             window.localStorage.removeItem('Tubes-2D3D.htm-' + id);
 
         }else{
