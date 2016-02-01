764a4933854578f66ce6855c5135fc57760dface
honzi <czechinthemail@gmail.com>
2016-01-31 18:09:24 -0800

    fixed window.setInterval() usage

--- js/tubes-2d3d.js
+++ js/tubes-2d3d.js
@@ -363,7 +363,7 @@ function setmode(newmode){
 
         animationFrame = window.requestAnimationFrame(draw);
         interval = window.setInterval(
-          'logic()',
+          logic,
           settings['ms-per-frame']
         );
 
