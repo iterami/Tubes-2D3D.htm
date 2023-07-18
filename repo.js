e2c8c408ba51313f1471f65adddd34fb2a36fb08
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 15:18:33 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,26 @@
 'use strict';
 
+function load_data(id){
+    speed = 10;
+    rotation = 0;
+
+    wall_splits = [
+      -2,
+      -2,
+      2,
+      -2,
+      -2,
+      2,
+      2,
+      2,
+    ];
+
+    colors = [
+      ['#f0f', '#06f', '#ff0', '#f60'],
+      ['#0f0', '#f00', '#00f', '#0ff'],
+    ];
+}
+
 function repo_drawlogic(){
     if(!colors[0]){
         return;
