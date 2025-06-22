cdcb171114ce2e67e7c18d4cc688870b8a3b1666
Jan Hořava <czechinthemail@gmail.com>
2025-06-22 10:29:53 -0700

    changed move key ids to underscores and text

--- repo.js
+++ repo.js
@@ -208,10 +208,10 @@ function repo_init(){
 }
 
 function repo_logic(){
-    let move_left = core_keys[core_storage_data['move-←']].state;
-    let move_right = core_keys[core_storage_data['move-→']].state;
-    let speed_down = core_keys[core_storage_data['move-↓']].state;
-    let speed_up = core_keys[core_storage_data['move-↑']].state;
+    let move_left = core_keys[core_storage_data.move_left].state;
+    let move_right = core_keys[core_storage_data.move_right].state;
+    let speed_down = core_keys[core_storage_data.move_down].state;
+    let speed_up = core_keys[core_storage_data.move_up].state;
     if(core_pointer.down_0){
         const x = core_pointer.x / globalThis.innerWidth;
         const y = core_pointer.y / globalThis.innerHeight;
