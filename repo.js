cc1074539d085d07a72595355af06cd94b627f65
Jan Hořava <czechinthemail@gmail.com>
2026-01-11 09:04:54 -0800

    changed load_data() to repo_load()

--- repo.js
+++ repo.js
@@ -1,26 +1,5 @@
 'use strict';
 
-function load_data(id){
-    speed = 10;
-    rotation = 0;
-
-    wall_splits = [
-      -2,
-      -2,
-      2,
-      -2,
-      -2,
-      2,
-      2,
-      2,
-    ];
-
-    colors = [
-      ['#f0f', '#06f', '#ff0', '#f60'],
-      ['#0f0', '#f00', '#00f', '#0ff'],
-    ];
-}
-
 function repo_drawlogic(){
     if(!colors[0]){
         return;
@@ -205,6 +184,27 @@ function repo_init(){
     canvas_init({
       'cursor': 'pointer',
     });
+}
+
+function repo_load(id){
+    speed = 10;
+    rotation = 0;
+
+    wall_splits = [
+      -2,
+      -2,
+      2,
+      -2,
+      -2,
+      2,
+      2,
+      2,
+    ];
+
+    colors = [
+      ['#f0f', '#06f', '#ff0', '#f60'],
+      ['#0f0', '#f00', '#00f', '#0ff'],
+    ];
 }
 
 function repo_logic(){
